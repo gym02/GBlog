8e2314fbbbc16a7639f78fe29d0242644639a48f
高垚淼 <663800595@qq.com>
2023-11-15 12:50:54 +0800

    上线

---
+++ blog-view/src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+	/**
+	 * @type {string}
+	 * @description 首页三张背景图
+	 */
+	// 原背景图
+	bg1: 'https://cdn.naccl.top/blog/img/bg1.jpg',
+	bg2: 'https://cdn.naccl.top/blog/img/bg2.jpg',
+	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
+	
+
+
+	/**
+	 * @type {string}
+	 * @description 首页故障风文字
+	 */
+	malfunctionText: 'Gavana\'s Blog'
+}
